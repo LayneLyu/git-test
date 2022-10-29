a10a013b8174f4d78aaa0c91b308dee0a8f977ee
LayneLyu <116560977+LayneLyu@users.noreply.github.com>
2022-10-29 22:38:37 +0800

    pull test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello git7");
         System.out.println("hello git8");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
